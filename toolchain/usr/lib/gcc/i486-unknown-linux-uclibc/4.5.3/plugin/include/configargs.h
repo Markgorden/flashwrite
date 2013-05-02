d3ef67c7cba25ba4ac3ddd9824c3914a6a63a75c
haojiesun <haojiesun@gmail.com>
2013-05-03 08:19:09 +1200

    added toolchain binaries

---
+++ toolchain/usr/lib/gcc/i486-unknown-linux-uclibc/4.5.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/work/ps2/mywork/buildroot/output/toolchain/gcc-4.5.3/configure --prefix=/work/ps2/mywork/buildroot/output/host/usr --build=i686-pc-linux-gnu --host=i686-pc-linux-gnu --target=i486-unknown-linux-uclibc --enable-languages=c,c++ --with-sysroot=/work/ps2/mywork/buildroot/output/host/usr/i486-unknown-linux-uclibc/sysroot --with-build-time-tools=/work/ps2/mywork/buildroot/output/host/usr/i486-unknown-linux-uclibc/bin --disable-__cxa_atexit --enable-target-optspace --disable-libgomp --with-gnu-ld --disable-libssp --disable-multilib --enable-tls --enable-shared --with-gmp=/work/ps2/mywork/buildroot/output/host/usr --with-mpfr=/work/ps2/mywork/buildroot/output/host/usr --with-mpc=/work/ps2/mywork/buildroot/output/host/usr --enable-threads --disable-decimal-float --with-arch=i486 --with-tune=i486 --with-pkgversion='Buildroot 2012.02' --with-bugurl=http://bugs.buildroot.net/";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i486" }, { "arch", "i486" }, { "tune", "i486" } };
